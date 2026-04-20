9d27fcfe2a4d4f7740dd8d439ef4dd7fbf9da373
paul-murithi <paulmurithi518@gmail.com>
2026-04-20 20:11:51 +0300

    chore: Token expiry to one hour

--- apps/api/src/services/auth.service.ts
+++ apps/api/src/services/auth.service.ts
@@ -80,7 +80,7 @@ export class AuthService {
       arguments: [
         token,
         merchantId,
-        "1000", // TODO: 80 for testing
+        "3600", // TODO: 80 for testing
       ],
     });
 
